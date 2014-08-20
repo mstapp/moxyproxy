b2b69a90298d0d706c186044e2f95c57f6f0b2a8
Mike Stapp <mstapp@tiggertail.com>
2014-08-20 11:15:17 -0400

    Proper error handling via node domain

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ var express = require('express'),
     config = require('./config'),
     _ = require('underscore'),
     fs = require('fs'),
+    domain = require('domain').create(),
     httpProxy = require('http-proxy'),
     mockery = require('./mockery');
 
 var app = express();
+
+// trap any errors on this domain, sync or async
+domain.on('error', function(err){
+    console.log(err);
+});
+
 
 // statics
 _.each(config.staticPaths, function(path){
@@ -13,21 +20,20 @@ _.each(config.staticPaths, function(path){
 });
 
 // proxy paths & mocks
+//
 var proxy = httpProxy.createProxyServer();
 _.each(config.proxyUrlPaths, function(basePath) {
     _.each('get,post,put,delete'.split(','), function(verb){
         app[verb](basePath + '*', function(req, res){
-            try {
+            // run in a domain to safely handle sync or async errors
+            domain.run(function() {
                 if (config.enableMocking && mockery.isMocked(req)) {
                     mockery.mock(req, res);
                 }
                 else {
                     proxy.web(req, res, { target: config.proxyTarget });
                 }
-            }
-            catch (err) {
-                console.log('Error during proxy call: ' + err);
-            }
+            });
         });
     });
 });
